5ad08937c96e60f77e0b4f452b8cdf6780057d3d
JM <jesuscamposmendoza815@gmail.com>
2023-10-12 21:14:57 -0600

    funcion die

---
+++ funciondie.php
@@ -0,0 +1,18 @@
+<?php
+
+echo 'Soy una función die(), despues de mí no habrá nada';
+die();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h3>Funcion die</h3>
+</body>
+</html>
