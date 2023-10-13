923755953c335ac2d972b8e9f79c83c05ff064fe
JM <jesuscamposmendoza815@gmail.com>
2023-10-12 20:05:08 -0600

    ciclo while

---
+++ bucles.php
@@ -0,0 +1,14 @@
+<?php
+
+  // For
+  for ($i = 1; $i <= 10; $i++) {
+    echo $i."\n";
+  }
+
+  // While
+  $limite = 0;
+  while ($limite <= 10) {
+    echo $limite.": while\n";
+    $limite++;
+  }
+?>
