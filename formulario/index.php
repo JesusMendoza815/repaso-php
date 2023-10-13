0479c1fbb339801c521b999687c41c2b49e8bda2
JM <jesuscamposmendoza815@gmail.com>
2023-10-13 16:26:34 -0600

    validar formulario

--- formulario/index.php
+++ formulario/index.php
@@ -1,12 +1,11 @@
 <?php
 
   if ($_POST || $_GET) {
-    $nombre = $_POST['nombre'];
-    $sexo = $_POST['sexo'];
-    $year = $_POST['year'];
-    $terms = $_POST['terms'];
+    $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    echo 'Hola '.$nombre." has enviado tu formulario por: ".$_SERVER['REQUEST_METHOD'];
+    echo 'Hola '.$nombre.' has enviado tu formulario por: '.$_SERVER['REQUEST_METHOD'].'<br/>';
+    echo 'Y tu correo es: '.$email;
   };
 
 ?>
@@ -22,22 +21,11 @@
 
 <body>
   <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+    <label for="nombre">Nombre: </label>
     <input type="text" placeholder="Ingresa tu nombre" name="nombre" required>
     <br>
-    <label for="sexo">Hombre</label>
-    <input type="radio" name="sexo" value="hombre" id="sexo" required>
-    <br>
-    <label for="sexo">Mujer</label>
-    <input type="radio" name="sexo" value="mujer" id="sexo" required>
-    <br>
-    <select name="year" id="year" required>
-      <option value="2000">2000</option>
-      <option value="2001">2001</option>
-      <option value="2002">2002</option>
-    </select>
-    <br>
-    <label for="terms">Terminos y condiciones</label>
-    <input type="checkbox" name="terms" id="terms" required>
+    <label for="email">Correo: </label>
+    <input type="email" placeholder="Ingresa tu correo" name="email" id="email" required>
     <br>
     <input type="submit" value="Enviar">
   </form>
